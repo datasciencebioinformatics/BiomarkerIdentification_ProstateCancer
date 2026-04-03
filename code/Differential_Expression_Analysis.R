3eadfac4b75e1115806f135b87fb041a40fdbe77
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2026-04-02 20:18:58 -0500

    Update Differential_Expression_Analysis.R

--- code/Differential_Expression_Analysis.R
+++ code/Differential_Expression_Analysis.R
@@ -1,6 +1,7 @@
 # Create DESeqDataSet from your prepared matrix
 dds <- DESeqDataSetFromMatrix(countData = read_counts_table,
                               colData = sample_sheet_data,
+                              design = ~ Tissue.Type)
 
 # Run DeSeq2
 dds <- DESeq(dds)
