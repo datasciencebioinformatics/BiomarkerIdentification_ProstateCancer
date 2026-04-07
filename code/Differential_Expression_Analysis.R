294497681037b5bc8b122391a1e6a666f1c20b66
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2026-04-07 03:57:20 -0500

    Update Differential_Expression_Analysis.R

--- code/Differential_Expression_Analysis.R
+++ code/Differential_Expression_Analysis.R
@@ -1,18 +1,40 @@
+########################################################################
+# First phase, Tissue.Type
 # Create DESeqDataSet from your prepared matrix
-dds <- DESeqDataSetFromMatrix(countData = read_counts_table,
+dds_tumor_normal <- DESeqDataSetFromMatrix(countData = read_counts_table,
                               colData = sample_sheet_data,
                               design = ~ Tissue.Type)
-
 # Run DeSeq2
-dds <- DESeq(dds)
+dds_tumor_normal <- DESeq(dds_tumor_normal)
 
 # Obtain the results
-res_tumor_normal <- results(dds, contrast=c("Tissue.Type","Tumor","Normal"))
+res_tumor_normal <- results(dds_tumor_normal, contrast=c("Tissue.Type","Tumor","Normal"))
 
 # Take 
 res_tumor_normal<-data.frame(res_tumor_normal[which(res_tumor_normal$padj<0.05 & abs(res_tumor_normal$log2FoldChange)>2),])
 
-# Write a data frame to Excel
-write_xlsx(res_tumor_normal,  paste(project_folder,"Supplemental_Table_S1.xlsx",sep="" ))
+
+########################################################################
+# Second phase, Tissue.Type
+# First phase, Tissue.Type
+# Create DESeqDataSet from your prepared matrix
+dds_primary_metastic <- DESeqDataSetFromMatrix(countData = read_counts_table,
+                              colData = sample_sheet_data,
+                              design = ~ Tumor.Descriptor)
+
+# Run DeSeq2
+dds_primary_metastic <- DESeq(dds_primary_metastic)
+
+# Obtain the results
+res_Primary_normal <- results(dds_primary_metastic, contrast=c("Tumor.Descriptor","Primary","Not Applicable"))
+res_Metastatic_normal <- results(dds_primary_metastic, contrast=c("Tumor.Descriptor","Metastatic","Not Applicable"))
+
+# Take 
+res_Primary_normal<-data.frame(res_Primary_normal[which(res_Primary_normal$padj<0.05 & abs(res_Primary_normal$log2FoldChange)>2),])
+res_Metastatic_normal<-data.frame(res_Metastatic_normal[which(res_Metastatic_normal$padj<0.05 & abs(res_Metastatic_normal$log2FoldChange)>2),])
+
+
+sheets_list <- list("tumor_genes"= res_tumor_normal, "primary_tumor_genes" = res_Primary_normal, "metastatic_tumor_genes"=res_Metastatic_normal)
+write_xlsx(sheets_list,paste(project_folder,"Supplemental_Table_S1.xlsx",sep="" ))
 
                               
