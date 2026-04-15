2c9ec1f8d29e2718934a542f5ad2fa5eb996ad2b
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2026-04-15 15:20:10 -0300

    Update Differential_Expression_Analysis.R

--- code/Differential_Expression_Analysis.R
+++ code/Differential_Expression_Analysis.R
@@ -13,8 +13,8 @@ res_tumor_normal <- results(dds_tumor_normal, contrast=c("Tissue.Type","Tumor","
 # Take 
 res_tumor_normal<-data.frame(res_tumor_normal[which(res_tumor_normal$padj<0.05 & abs(res_tumor_normal$log2FoldChange)>2),])
 ########################################################################
-
 # 2. Save the object to an .rds file
-saveRDS(dds_tumor_normal, file = paste(output_dir,"dds_tumor_normal.rds",sep=""))
-saveRDS(res_tumor_normal, file = paste(output_dir,"res_tumor_normal",sep=""))
+saveRDS(dds_tumor_normal, file = file.path(project_folder, "rsd","rsd","/dds_tumor_normal.rsd" ))
+saveRDS(res_tumor_normal, file = file.path(project_folder, "rsd",rsd,"/res_tumor_normal.rsd" ))
+
 
