ac75fcd8a0e9b3494a4b3c80033d18b1ae8e21c4
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2026-04-17 18:07:32 -0300

    Update Biomarkers_BaysianNetwork.R

--- code/Biomarkers_BaysianNetwork.R
+++ code/Biomarkers_BaysianNetwork.R
@@ -1,16 +1,4 @@
 #########################################################################################################
-# Make data discrete
-
-### discretize all numeric columns differently
-read_counts_table_tpm_disc <- discretizeDF(read_counts_table_tpm[rownames(res_tumor_normal),], default = list(
-  method = "interval", breaks = 3,
-  labels = c("low","medium", "high")
-))
-
-# Add tisue type to data.frame
-read_counts_table_tpm_disc<-cbind(t(read_counts_table_tpm_disc),Tissue_Type=sample_sheet_data[colnames(read_counts_table_tpm_disc),"Tissue.Type"])
-
-#########################################################################################################
 # Make the read_counts_table_tpm_disc a data.frame
 read_counts_table_tpm_disc<-data.frame(read_counts_table_tpm_disc)
 
@@ -39,33 +27,25 @@ png(filename=paste(output_dir,"Bayesian_Network_structure_Tissue_Type.png",sep="
   plot(as.igraph(bn_cancer_data), layout=layout_with_fr, vertex.label =correspondence_table_bck[V(as.igraph(bn_cancer_data))$name,"gene_name"], vertex.size = 20)
 dev.off()
 #########################################################################################################
-set.seed(123) # Define a semente para reprodutibilidade
-sample_ids_trainning<-sample(rownames(read_counts_table_tpm_disc), dim(read_counts_table_tpm_disc)[1]*0.75)
-sample_ids_testing  <-rownames(read_counts_table_tpm_disc)[!rownames(read_counts_table_tpm_disc) %in% sample_ids_trainning]
 
 # Create bayesian networks
-bn_cancer_data                      <- tabu(data.frame(read_counts_table_tpm_disc))
-bn_cancer_data_training_testing_set <- tabu(data.frame(read_counts_table_tpm_disc[sample_ids_trainning,]))
+bn_cancer_data_trainning             <- tabu(data.frame(read_counts_table_tpm_disc[sample_ids_testing,]))
+bn_cancer_data_testing_set           <- tabu(data.frame(read_counts_table_tpm_disc[sample_ids_trainning,]))
 
 
 # Create bayesian networks
-bn_cancer_data                      = bn.fit(bn_cancer_data, data.frame(read_counts_table_tpm_disc),method = "mle")
-bn_cancer_data_training_testing_set = bn.fit(bn_cancer_data_training_testing_set, data.frame(read_counts_table_tpm_disc[sample_ids_trainning,]),method = "mle")
+bn_cancer_data                      = bn.fit(bn_cancer_data_trainning,   data.frame(read_counts_table_tpm_disc[sample_ids_testing,]),method = "mle")
+bn_cancer_data_training_testing_set = bn.fit(bn_cancer_data_testing_set, data.frame(read_counts_table_tpm_disc[sample_ids_trainning,]),method = "mle")
 
 
 # Predicted data - Separate trainning versus testing
-predicted_training             <- predict(bn_cancer_data, node = "Tissue_Type", data = data.frame(read_counts_table_tpm_disc), method = "parents")
-predicted_training_testing_set <- predict(bn_cancer_data_training_testing_set, node = "Tissue_Type", data = data.frame(read_counts_table_tpm_disc[sample_ids_testing,]), method = "parents")
+predictions             <- predict(bn_cancer_data, node = "Tissue_Type", data =  data.frame(read_counts_table_tpm_disc[sample_ids_trainning,]), method = "parents")
+
+
+# Confusion Matrix
+conf_matrix <- table(Actual = as.vector((read_counts_table_tpm_disc[sample_ids_testing,"Tissue_Type"])), Predicted = as.vector(predictions))
+# Accuracy calculation
+accuracy <- sum(diag(conf_matrix)) / sum(conf_matrix)
+print(accuracy)
                                   
 
-results_trainning <- confusionMatrix(data = factor(predicted_training), reference =  factor(data.frame(read_counts_table_tpm_disc)$Tissue_Type))
-results_trainning_testing <- confusionMatrix(data = factor(predicted_training_testing_set), reference =  factor(data.frame(read_counts_table_tpm_disc[sample_ids_testing,])$Tissue_Type))
-
-write.table(data.frame(results_trainning$overall), file = paste(output_dir,"confusionMatrix_bn_all.txt",sep=""),, sep = "\t", row.names = TRUE, append=FALSE)
-write.table(data.frame(results_trainning$table), file = paste(output_dir,"confusionMatrix_bn_all.txt",sep=""),, sep = "\t", row.names = TRUE, append=TRUE)
-
-write.table(data.frame(results_trainning_testing$overall), file = paste(output_dir,"confusionMatrix_bn_testing.txt",sep=""),, sep = "\t", row.names = TRUE, append=FALSE)
-write.table(data.frame(results_trainning_testing$table), file = paste(output_dir,"confusionMatrix_bn_testing.txt",sep=""),, sep = "\t", row.names = TRUE, append=TRUE)
-
-
-#########################################################################################################
