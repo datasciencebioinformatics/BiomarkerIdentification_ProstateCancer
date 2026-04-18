be92511be7831ba66588410505572a8812d0f57f
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2026-04-18 09:36:22 -0300

    Update Biomarkers_DecisionTree.R

--- code/Biomarkers_DecisionTree.R
+++ code/Biomarkers_DecisionTree.R
@@ -1,3 +1,38 @@
+#########################################################################################################
+# Start data.frame
+df_read_counts_table_tpm_disc          <- data.frame(matrix("", nrow = length(rownames(res_tumor_normal)), ncol = length(colnames(read_counts_table_tpm))))
+df_read_counts_table_tpm_disc_no_label <- data.frame(matrix("", nrow = length(rownames(res_tumor_normal)), ncol = length(colnames(read_counts_table_tpm))))
+
+# Set rownames()
+rownames(df_read_counts_table_tpm_disc)<-rownames(res_tumor_normal)
+rownames(df_read_counts_table_tpm_disc_no_label)<-rownames(res_tumor_normal)
+
+# Remove first collumn
+colnames(df_read_counts_table_tpm_disc)<-colnames(read_counts_table_tpm)
+colnames(df_read_counts_table_tpm_disc_no_label)<-colnames(read_counts_table_tpm)
+
+# Make data discrete
+# For each gene 
+for (gene in rownames(res_tumor_normal))
+{
+	# Take the discrete value
+ 	a=cut(as.vector(unlist(as.vector(read_counts_table_tpm[gene,]))), breaks = 3,labels = c("low","medium", "high") , include.lowest = TRUE)
+ 	b=cut(as.vector(unlist(as.vector(read_counts_table_tpm[gene,]))), breaks = 3, include.lowest = TRUE)
+
+	# add to vector
+	df_read_counts_table_tpm_disc[gene,colnames(read_counts_table_tpm)]<-a
+
+ 	# add to vector
+ 	df_read_counts_table_tpm_disc_no_label[gene,colnames(read_counts_table_tpm)]<-b 
+}
+
+# Add tisue type to data.frame
+read_counts_table_tpm_disc<-data.frame(cbind(t(read_counts_table_tpm[rownames(res_tumor_normal),]),Tissue_Type=sample_sheet_data[colnames(read_counts_table_tpm),"Tissue.Type"]))
+
+# Set the Tissue_Type collumn as factor
+read_counts_table_tpm_disc$Tissue_Type<-as.factor(read_counts_table_tpm_disc$Tissue_Type)
+
+#########################################################################################################
 # Compute rpart model 
 mreSummary <- function(data, lev = NULL, model = NULL) {
 rmse=rmse(data$obs, data$pred) 
@@ -6,13 +41,6 @@ mre=mre(data$obs, data$pred)
 cor=cor(data$obs, data$pred)
 c(MRE = mre, RMSE=rmse, MAE = mae, Cor=cor)
 }
-
-#########################################################################################################
-# Add tisue type to data.frame
-read_counts_table_tpm_disc<-data.frame(cbind(t(read_counts_table_tpm[rownames(res_tumor_normal),]),Tissue_Type=sample_sheet_data[colnames(read_counts_table_tpm),"Tissue.Type"]))
-
-# Set the Tissue_Type collumn as factor
-read_counts_table_tpm_disc$Tissue_Type<-as.factor(read_counts_table_tpm_disc$Tissue_Type)
 #########################################################################################################
 # Train the model using the 'rpart' method
 model_comb <-  caret::train(
